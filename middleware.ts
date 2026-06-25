84425e758961b3a26e800754b77e7035d539cfbd
0camm <garypaytonpg@gmail.com>
2026-06-24 20:36:55 -0400

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,5 @@
-import { auth } from '@/lib/auth'
-import { NextResponse } from 'next/server'
-
-export default auth((req) => {
-  const { pathname } = req.nextUrl
-  if (pathname.startsWith('/admin') && !pathname.startsWith('/admin/login')) {
-    if (!req.auth) {
-      return NextResponse.redirect(new URL('/admin/login', req.url))
-    }
-  }
-  return NextResponse.next()
-})
+export { auth as middleware } from '@/lib/auth'
 
 export const config = {
-  matcher: ['/admin/:path*', '/api/admin/:path*'],
+  matcher: ['/admin/:path*'],
 }
